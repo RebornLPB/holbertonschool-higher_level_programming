ffa112e5ace70313e44a07d2826ec341af714e65
Dylan <reborndiscord@gmx.fr>
2026-09-22 10:30:41 +0200

    Add initial README files and implement basic serialization functions

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,10 @@
+import pickle
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(data, f)
+
+def load_and_deserialize(filename):
+    with open(filename, "rb") as f:
+        pickle.load(f)
