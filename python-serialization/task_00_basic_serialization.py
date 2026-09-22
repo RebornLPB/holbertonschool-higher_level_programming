6b65ff1660a344809cd09944e803ea134be21d90
Dylan <reborndiscord@gmx.fr>
2026-09-22 10:40:27 +0200

    Fix file opening modes in serialization functions to use text mode

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,9 +2,10 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
-    with open(filename, "wb") as f:
+    with open(filename, "w") as f:
         json.dump(data, f)
 
+
 def load_and_deserialize(filename):
-    with open(filename, "rb") as f:
+    with open(filename, "r") as f:
         return json.load(f)
